8d0a7706184ae662032903b99852464e7bac2850
mihon.testosterone <mihon.testosterone@gmail.com>
2021-08-26 16:59:52 +0300

    initial

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+import { Component } from "react";
+import Searchbar from "./components/Searchbar/Searchbar";
+
+export default class App extends Component {
+  state = {
+    pics: null,
+  };
+
+  componentDidMount() {
+    const key = "23098764-6c28342abea29650d4f55356c";
+    let url = `https://pixabay.com/api/?q=flower&page=1&key=${key}&image_type=photo&orientation=horizontal&per_page=12`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((pics) => this.setState({ pics: pics.hits }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Searchbar />
+      </div>
+    );
+  }
+}
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+// import PropTypes from "prop-types";
+
+export default class Searchbar extends Component {
+  render() {
+    return (
+      <header className="Searchbar">
+        <form className="SearchForm">
+          <button type="submit" className="SearchForm-button">
+            <span className="SearchForm-button-label">Search</span>
+          </button>
+
+          <input
+            className="SearchForm-input"
+            type="text"
+            autocomplete="off"
+            autofocus
+            placeholder="Search images and photos"
+          />
+        </form>
+      </header>
+    );
+  }
+}
